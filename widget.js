1b4b481c8158067dab4ff95822143e9deb06706f
Felipe Ferreira <felipe.ferreira@free.fr>
2018-07-26 06:49:52 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -229,7 +229,8 @@ cpdefine("inline:com-chilipeppr-widget-myhomecnc", ["chilipeppr_ready", "CHART" 
                 }
             };
             
-            var ctx = $('#' + this.id + ' .object-temperature-chart').get(0).getContext("2d");
+            var ctx = $('#object-temperature-chart').get(0).getContext("2d");
+            //var ctx = $('#' + this.id + ' .object-temperature-chart').get(0).getContext("2d");
             var myLine = new Chart(ctx, config);
         },
         
