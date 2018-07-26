93f9667a1ddf5b898e6e84eca3e7eb734c5f1381
Felipe Ferreira <felipe.ferreira@free.fr>
2018-07-26 07:37:55 +0000

    Made some changes to ChiliPeppr widget using Cloud9

--- widget.js
+++ widget.js
@@ -27,9 +27,9 @@ requirejs.config({
         // Example of how to define the key (you make up the key) and the URL
         // Make sure you DO NOT put the .js at the end of the URL
         // SmoothieCharts: '//smoothiecharts.org/smoothie',
-        ko: '//cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
-        Chart: 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min',
-        socketio: '//cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io'
+        KO: 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min',
+        CHART: 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min',
+        socketio: 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io'
     },
     shim: {
         // See require.js docs for how to define dependencies that
